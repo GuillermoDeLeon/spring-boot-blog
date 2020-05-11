78db24b693d89e4c04d15adbad0f1269fa354ff7
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-05-11 12:20:36 -0500

    Spring controllers exercise

---
+++ src/main/java/com/codeup/springbootblog/PostController.java
@@ -0,0 +1,37 @@
+package com.codeup.springbootblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts() {
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postsId(@PathVariable String id) {
+        return "view an individual post " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String postsCreate() {
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String submitCreatePosts() {
+        return "create a new post";
+    }
+}
+
+
